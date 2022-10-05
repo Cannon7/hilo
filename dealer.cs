ca847ed9f0aa3ec00937a7bf7787fe19034cc7b0
KasonMills <kmillsventura@byui.edu>
2022-10-04 18:46:21 -0600

    Co-authored-by: corychoate <corychoate@users.noreply.github.com>

--- dealer.cs
+++ dealer.cs
@@ -5,53 +5,37 @@ using System.Threading;
 namespace week3;
 
 
-public class Main
+public class Dealer
 {
 //main function
 
 
-public void mainline()
+public void playgame() 
 {
+
+    bool play_again = true;
     do
     {
-        bool play_again = true;
-
-        while (play_again)
-        {
-
-            Console.WriteLine("The card is: "); // number to be displayed after card class is complete
-            Console.WriteLine("Higher or lower? [h/l] ");
-            Console.ReadLine();
-            Console.WriteLine("Next card was: "); // also needed from card class
-            Console.WriteLine("Your score is: "); // enter from score function
-            Console.WriteLine("Play again? [y/n] ");     
-            string continue_game = Console.ReadLine();
+        
+        Console.WriteLine("The card is: "); // number to be displayed after card class is complete
+        Console.WriteLine("Higher or lower? [h/l] ");
+        Console.ReadLine();
+        Console.WriteLine("Next card was: "); // also needed from card class
+        Console.WriteLine("Your score is: "); // enter from score function
+        Console.WriteLine("Play again? [y/n] ");     
+        string continue_game = Console.ReadLine();
             if (continue_game == "y")
             {
                 play_again = true;
-                }
+            }
             else if (continue_game == "n")
             {
                 play_again = false;
             }
-            
-            
-        }
-        
-    }
+                   
+    } while (play_again == true);
     
     
 }
 
-//display function (display rules of game)
-
-
-
-//score function(keeps track of score as game is played)
-
-
-
-
-   
-            
 }
